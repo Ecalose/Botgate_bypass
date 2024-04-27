5b86aa13d63ff818e199f4a850042e989de67d60
R0A1NG <1793486547@qq.com>
2024-04-27 12:17:47 +0800

    update

--- mitmdump.py
+++ mitmdump.py
@@ -3,100 +3,18 @@ import aiohttp
 import time
 import base64
 
-
 flag_req = None
-
-jstext = '''if (typeof window.globalSocket121 !== 'undefined') {
-    if (window.globalSocket121.readyState === WebSocket.CLOSED) {
-        socket_start()
-    }
-} else {
-    socket_start()
-}
-function socket_start(){
-    const socket121 = new WebSocket('ws://127.0.0.1:8765');
-    window.globalSocket121 = socket121;
-    socket121.onopen = function(event) {
-        socket121.send('password=123456');
-    };
-    socket121.onmessage = function(event) {
-        const receivedMessage = atob(event.data);
-        const parts = receivedMessage.split("------------");
-        const varChars = parts[0]
-        const reqdata = parts[1]
-        if (parts.length == 2) {
-            try {
-                let [newdata1, newdata2, newdata3, newdata4, newdata5] = reqdata.split("[][][][][][]");
-                newdata1 = atob(newdata1);
-                newdata2 = atob(newdata2);
-                newdata3 = atob(newdata3);
-                newdata4 = atob(newdata4);
-                newdata5 = atob(newdata5);
-                const fetchOptions = {};
-                const headers = {};
-                if (newdata5 !== undefined && newdata5 !== null && newdata5 !== '') {
-                    const headersArray = newdata5.split('|||').map(header => header.trim().split(': '));
-                    headersArray.forEach(([key, value]) => {
-                        headers[key] = value;
-                    });
-                }
-                fetchOptions["method"] = newdata1;
-                fetchOptions["body"] = newdata4;
-                if (Object.keys(headers).length > 0) {
-                    fetchOptions.headers = new Headers(headers);
-                }
-                fetch(newdata2, fetchOptions)
-                .then(response => {
-                    const statusCode = response.status.toString(); 
-                    const headersJson = {};
-                    response.headers.forEach((value, name) => {
-                        headersJson[name] = value;
-                    });
-                    headersString = JSON.stringify(headersJson);
-
-                    xydata = response.text();
-
-                    return xydata.then(xydata => {
-                        return {
-                            statusCode: statusCode,
-                            headersString: headersString,
-                            xydata: xydata
-                        };
-                    });
-                })
-                .then(({ statusCode, headersString, xydata }) => {
-                    var encoder = new TextEncoder();
-                    var encodedUint8Array = encoder.encode(xydata);
-                    var xydata = arrayBufferToBase64(encodedUint8Array);
-                    function arrayBufferToBase64(arrayBuffer) {
-                        var binary = '';
-                        var bytes = new Uint8Array(arrayBuffer);
-                        var len = bytes.byteLength;
-                        for (var i = 0; i < len; i++) {
-                            binary += String.fromCharCode(bytes[i]);
-                        }
-                        return binary;
-                    }
-                    socket121.send(btoa(varChars + '------------' + btoa(statusCode) + '------------' + btoa(headersString) + '------------' + btoa(xydata)));
-                })
-                .catch(error => {
-                    socket121.send(btoa(varChars + '------------' + btoa(0) + '------------' + btoa(0) + '------------' + btoa(0)));
-                });
-            } catch (error) {
-                socket121.send(btoa(varChars + '------------' + btoa(0) + '------------' + btoa(0) + '------------' + btoa(0)));
-            }
-        }
-    };
-}'''
+jstext = 'function socket_start(){const i=new WebSocket("ws://127.0.0.1:8765");(window.globalSocket121=i).onopen=function(t){i.send("password=123456")},i.onmessage=function(t){t=atob(t.data).split("------------");const a=t[0];var e=t[1];if(2==t.length)try{var[o,n,s,r,d]=e.split("[][][][][][]"),o=atob(o),n=atob(n),r=(atob(s),atob(r)),d=atob(d),b={};const c={};null!=d&&""!==d&&d.split("|||").map(t=>t.trim().split(": ")).forEach(([t,e])=>{c[t]=e}),b.method=o,b.body=r,0<Object.keys(c).length&&(b.headers=new Headers(c)),fetch(n,b).then(t=>{const e=t.status.toString(),o={};return t.headers.forEach((t,e)=>{o[e]=t}),headersString=JSON.stringify(o),(xydata=t.text()).then(t=>({statusCode:e,headersString:headersString,xydata:t}))}).then(({statusCode:t,headersString:e,xydata:o})=>{o=function(t){for(var e="",o=new Uint8Array(t),a=o.byteLength,n=0;n<a;n++)e+=String.fromCharCode(o[n]);return e}((new TextEncoder).encode(o));i.send(btoa(a+"------------"+btoa(t)+"------------"+btoa(e)+"------------"+btoa(o)))}).catch(t=>{i.send(btoa(a+"------------"+btoa(0)+"------------"+btoa(0)+"------------"+btoa(0)))})}catch(t){i.send(btoa(a+"------------"+btoa(0)+"------------"+btoa(0)+"------------"+btoa(0)))}}}void 0!==window.globalSocket121&&window.globalSocket121.readyState!==WebSocket.CLOSED||socket_start();'
 
 
 class CustomResponse:
+
     def __init__(self):
         self.num = 0
 
     async def request(self, flow: http.HTTPFlow) -> None:
         global flag_req
-        flag_req = flow.request.headers.get("Req-flag", "")
+        flag_req = flow.request.headers.get('Req-flag', '')
         if flag_req:
             await self.handle_delayed_request(flow)
 
@@ -104,70 +22,56 @@ class CustomResponse:
         url = flow.request.url
         method = flow.request.method
         headers = flow.request.headers
-        newheaders = "|||".join([f"Res-flag: {value}" if key == "Req-flag" else f"{key}: {value}" for key, value in headers.items()])
-        content_type = flow.request.headers.get("Content-Type", "")
-
-        if "application/json" in content_type:
+        newheaders = '|||'.join(
+            [f'Res-flag: {value}' if key == 'Req-flag' else f'{key}: {value}' for (key, value) in headers.items()])
+        content_type = flow.request.headers.get('Content-Type', '')
+        if 'application/json' in content_type:
             data1 = flow.request.text
-            reqtype = "json"
-        elif "application/x-www-form-urlencoded" in content_type:
+            reqtype = 'json'
+        elif 'application/x-www-form-urlencoded' in content_type:
             data1 = flow.request.text
-            reqtype = "xform"
-        elif "multipart/form-data" in content_type:
-            if "form-data" not in str(flow.request.text):
-                data1 = ""
+            reqtype = 'xform'
+        elif 'multipart/form-data' in content_type:
+            if 'form-data' not in str(flow.request.text):
+                data1 = ''
             else:
                 data1 = flow.request.text
-            reqtype = "formdata"
+            reqtype = 'formdata'
         else:
-            reqtype = ""
-            data1 = ""
-
-        burp0_json = {
-            "data": base64.b64encode(data1.encode()).decode(),
-            "header": base64.b64encode(newheaders.encode()).decode(),
-            "method": str(method),
-            "type": reqtype,
-            "url": base64.b64encode(url.encode()).decode()
-        }
+            reqtype = ''
+            data1 = ''
+        burp0_json = {'data': base64.b64encode(data1.encode()).decode(),
+                      'header': base64.b64encode(newheaders.encode()).decode(), 'method': str(method), 'type': reqtype,
+                      'url': base64.b64encode(url.encode()).decode()}
         try:
             async with aiohttp.ClientSession() as session:
-                async with session.post("http://127.0.0.1:3000/api",
-                                        headers={"Content-Type": "application/json;charset=UTF-8"},
+                async with session.post('http://127.0.0.1:3000/api',
+                                        headers={'Content-Type': 'application/json;charset=UTF-8'},
                                         json=burp0_json) as response:
                     aa = http.Headers()
-                    for key, value in response.headers.items():
+                    for (key, value) in response.headers.items():
                         aa.add(key, value)
-                    response2 = http.Response(
-                        http_version=b"HTTP/1.1",
-                        status_code=response.status,
-                        reason=response.reason.encode(),
-                        headers=aa,
-                        content=await response.read(),
-                        trailers=None,
-                        timestamp_start=time.time(),
-                        timestamp_end=time.time()
-                    )
+                    response2 = http.Response(http_version=b'HTTP/1.1', status_code=response.status,
+                                              reason=response.reason.encode(), headers=aa,
+                                              content=await response.read(), trailers=None, timestamp_start=time.time(),
+                                              timestamp_end=time.time())
                     flow.response = response2
         except:
-            flow.response.content = "flask服务器请求失败，检查server.py是否启动"
+            flow.response.content = 'flask服务器请求失败，检查server.py是否启动'
 
     def response(self, flow: http.HTTPFlow) -> None:
-        global jstext, flag_req
-        resflag = flow.request.headers.get("Res-Flag", "")
-        flag_req = flow.request.headers.get("Req-flag", "")
-        if not flag_req and not resflag:
-            content_type = flow.response.headers.get("Content-Type", "").lower()
+        global jstext
+        resflag = flow.request.headers.get('Res-Flag', '')
+        flag_req = flow.request.headers.get('Req-flag', '')
+        if not flag_req and (not resflag):
+            content_type = flow.response.headers.get('Content-Type', '').lower()
             if flow.response.text:
-                if content_type.startswith("application/javascript"):
+                if content_type.startswith('application/javascript'):
                     flow.response.text += jstext
-                if content_type.startswith("text/html"):
-                    flow.response.text += "<script>" + jstext + "</script>"
-            if "Content-Security-Policy" in flow.response.headers:
-                del flow.response.headers["Content-Security-Policy"]
+                if content_type.startswith('text/html'):
+                    flow.response.text += '<script>' + jstext + '</script>'
+            if 'Content-Security-Policy' in flow.response.headers:
+                del flow.response.headers['Content-Security-Policy']
 
 
-addons = [
-    CustomResponse()
-]
-
+addons = [CustomResponse()]
--- server.py
+++ server.py
@@ -1,11 +1,3 @@
-#!/usr/bin/python3
-# coding:utf-8
-
-# @Time    : 2024/4/9 15:37
-# @Author  : E0tk1
-# @File    : server.py
-# @IDE     : PyCharm
-
 import json
 import base64
 import asyncio
@@ -20,41 +12,37 @@ import string
 
 def generate_random_string(length=32):
     characters = string.ascii_lowercase + string.digits
-    return ''.join(random.choice(characters) for _ in range(length))
+    return ''.join((random.choice(characters) for _ in range(length)))
 
 
 def req_handle(verChar):
-    print("-----------------------------------------------------------------------------------------------")
+    print('-----------------------------------------------------------------------------------------------')
     try:
-        hmethod = request.get_json()["method"]
-        hurl = request.get_json()["url"]
+        hmethod = request.get_json()['method']
+        hurl = request.get_json()['url']
         hurl = base64.b64decode(hurl).decode()
-        htype = request.get_json()["type"]
+        htype = request.get_json()['type']
         try:
-            hheader = request.get_json()["header"]
+            hheader = request.get_json()['header']
             hheader = base64.b64decode(hheader).decode()
         except:
-            hheader = ""
-        hdata = request.get_json()["data"]
-        data = "{}[][][][][][]{}[][][][][][]{}[][][][][][]{}[][][][][][]{}".format(
-            base64.b64encode(hmethod.encode()).decode(),
-            base64.b64encode(hurl.encode()).decode(),
-            base64.b64encode(htype.encode()).decode(),
-            hdata,
-            base64.b64encode(hheader.encode()).decode()
-        )
-        print("请求信息：")
-        print("Method：{}".format(hmethod))
-        print("URL：{}".format(hurl))
-        print("Content-type：{}".format(htype))
-        print("Headers：{}".format(hheader))
-        print("Data：{}".format(base64.b64decode(hdata).decode()))
-        data = verChar + "------------" + str(data)
+            hheader = ''
+        hdata = request.get_json()['data']
+        data = '{}[][][][][][]{}[][][][][][]{}[][][][][][]{}[][][][][][]{}'.format(
+            base64.b64encode(hmethod.encode()).decode(), base64.b64encode(hurl.encode()).decode(),
+            base64.b64encode(htype.encode()).decode(), hdata, base64.b64encode(hheader.encode()).decode())
+        print('请求信息：')
+        print('Method：{}'.format(hmethod))
+        print('URL：{}'.format(hurl))
+        print('Content-type：{}'.format(htype))
+        print('Headers：{}'.format(hheader))
+        print('Data：{}'.format(base64.b64decode(hdata).decode()))
+        data = verChar + '------------' + str(data)
         data = base64.b64encode(data.encode()).decode()
         return data
     except:
-        print("发送给web客户端的消息：\ndata数据错误")
-        print("-----------------------------------------------------------------------------------------------")
+        print('发送给web客户端的消息：\ndata数据错误')
+        print('-----------------------------------------------------------------------------------------------')
         return None
 
 
@@ -65,8 +53,7 @@ except:
     app.config['JSON_AS_ASCII'] = False
 connected_clients = set()
 loop = None
-
-message = ""
+message = ''
 last_connect = None
 
 
@@ -75,16 +62,16 @@ def receive_data():
     verChar = str(generate_random_string())
     data = req_handle(verChar)
     if not data:
-        return "data数据错误"
+        return 'data数据错误'
     if loop is not None and last_connect:
         loop.call_soon_threadsafe(send_data_to_client, last_connect, data)
     else:
-        print("ws客户端未连接")
-        return "ws客户端未连接"
+        print('ws客户端未连接')
+        return 'ws客户端未连接'
     start_time = time.time()
     while time.time() - start_time < 2:
         try:
-            messages1 = message.split("------------")
+            messages1 = message.split('------------')
             if messages1 == ['']:
                 continue
             newmessages0 = messages1[0]
@@ -92,49 +79,44 @@ def receive_data():
             newmessages2 = base64.b64decode(messages1[2]).decode()
             newmessages3 = base64.b64decode(messages1[3]).decode()
             if verChar == newmessages0:
-                print("响应信息：")
-                print("Code：" + str(newmessages1))
-                print("Headers：" + newmessages2)
-                print("Data：" + newmessages3)
-                print("-----------------------------------------------------------------------------------------------")
-
-                if newmessages2 == "0" and newmessages3 == "0":
-                    return "网站访问失败，请检查\n1、URL是否正确！\n2、是否存在跨域访问\n3、网站是否能正常访问"
+                print('响应信息：')
+                print('Code：' + str(newmessages1))
+                print('Headers：' + newmessages2)
+                print('Data：' + newmessages3)
+                print('-----------------------------------------------------------------------------------------------')
+                if newmessages2 == '0' and newmessages3 == '0':
+                    return '网站访问失败，请检查\n1、URL是否正确！\n2、是否存在跨域访问\n3、网站是否能正常访问'
                 newheaders = json.loads(newmessages2)
-
                 response = make_response(newmessages3, int(newmessages1))
-                for key, value in newheaders.items():
-                    if key == "transfer-encoding":
+                for (key, value) in newheaders.items():
+                    if key == 'transfer-encoding':
                         continue
                     response.headers[key] = value
                 return response
         except:
             time.sleep(0.1)
-    print("发送给web客户端的消息：\n发送给了ws客户端，但是没有返回！\n请检查：\n1、网站访问时间是否超过2秒\n2、ws客户端是否断开连接")
-    print("-----------------------------------------------------------------------------------------------")
-    return "发送给了ws客户端，但是没有返回！\n请检查：\n1、网站访问时间是否超过2秒\n、ws客户端是否断开连接"
+    print(
+        '发送给web客户端的消息：\n发送给了ws客户端，但是没有返回！\n请检查：\n1、网站访问时间是否超过2秒\n2、ws客户端是否断开连接')
+    print('-----------------------------------------------------------------------------------------------')
+    return '发送给了ws客户端，但是没有返回！\n请检查：\n1、网站访问时间是否超过2秒\n、ws客户端是否断开连接'
 
 
 async def handle_client(websocket, path):
     global message, last_connect
     last_connect = websocket
     connected_clients.add(websocket)
-
     token = await websocket.recv()
     if token != 'password=123456':
         await websocket.close()
         return
-
     client_address = websocket.remote_address
-
     try:
         headers = websocket.request_headers
         oriin = headers.get('Origin')
     except:
-        oriin = ""
-    print("ws客户端连接成功，IP：{}，端口：{}，所在站点域名：{}".format(client_address[0], client_address[1], oriin))
-    await websocket.send(base64.b64encode("success!".encode()).decode())
-
+        oriin = ''
+    print('ws客户端连接成功，IP：{}，端口：{}，所在站点域名：{}'.format(client_address[0], client_address[1], oriin))
+    await websocket.send(base64.b64encode('success!'.encode()).decode())
     while True:
         try:
             message = await websocket.recv()
@@ -155,13 +137,13 @@ def start_ws_server():
     global loop
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    server = websockets.serve(handle_client, "0.0.0.0", 8765)
+    server = websockets.serve(handle_client, '0.0.0.0', 8765)
     loop.run_until_complete(server)
     loop.run_forever()
 
 
 def start_flask_app():
-    app.run("127.0.0.1", port=3000)
+    app.run('127.0.0.1', port=3000)
 
 
 if __name__ == '__main__':
